d30af44faf608937bc4f7f26062d9aaf9580ec62
Pereirapedr0 <132013108+Pereirapedr0@users.noreply.github.com>
2024-02-07 20:32:22 -0300

    Co-authored-by: TyAssis <TyAssis@users.noreply.github.com>

--- betting-pool/randomizer.js
+++ betting-pool/randomizer.js
@@ -1,26 +1,60 @@
-const rodadas = [];
+const rodadasGeral = [];
 
-function gerarRodada(rodadas) {
-    const rodada = []
+function gerarRodada(rodadasAteAgora) {
+    const rodadaSendoConstruida = []
     const times = [1, 2, 3, 4];
     const timesJaNaRodada = [];
 
     for (let i = 0; i < times.length; i++) {
         for (let j = i + 1; j < times.length; j++) {
             // i = 0; j = 2
-            if (!timesJaNaRodada.includes(times[i]) && !timesJaNaRodada.includes(times[j])) { 
-                rodada.push([times[i], times[j]]);
-                timesJaNaRodada.push(times[i]);
-                timesJaNaRodada.push(times[j]);
+            const timesNaoEstaoNaRodada = !timesJaNaRodada.includes(times[i]) && !timesJaNaRodada.includes(times[j]);
+            if (timesNaoEstaoNaRodada) {
+                // verificar se uma partida ja aconteceu antes em outra rodada
+
+                if (rodadasAteAgora.length === 0) {
+                    rodadaSendoConstruida.push([times[i], times[j]]);
+                    timesJaNaRodada.push(times[i]);
+                    timesJaNaRodada.push(times[j]);
+                } else {
+                    rodadasAteAgora.forEach((rodadaAnterior) => {
+                        const partida = [times[i], times[j]];
+                        const jaAconteceu = rodadaAnterior.find(partidaAnterior => JSON.stringify(partidaAnterior) === JSON.stringify(partida));
+                        if (!jaAconteceu) {
+                            console.log("partida Nova");
+                            console.log(partida);
+                            rodadaSendoConstruida.push([times[i], times[j]]);
+                            timesJaNaRodada.push(times[i]);
+                            timesJaNaRodada.push(times[j]);
+                        }
+                    })
+                }
+
+                /**
+                 * rodadas [
+                 *  'rodada' [
+                 *    'partidas' [1, 2] [3, 4]
+                 *  ],
+                 *  'rodada' [
+                 *    'partidas' [1, 3] [2, 4]
+                 *  ]
+                 * ]
+                 * 
+                 */
+
+
+                // ---
+
+
             }
         }
     }
 
-    return rodada;
+    return rodadaSendoConstruida;
 }
 
-const rodada1 = gerarRodada(rodadas);
-rodadas.push(rodada1);
+const rodada1 = gerarRodada(rodadasGeral);
+rodadasGeral.push(rodada1);
 
 const rodadaEsperada1 = [[1, 2], [3, 4]];
 if (JSON.stringify(rodadaEsperada1) === JSON.stringify(rodada1)) {
@@ -30,14 +64,24 @@ if (JSON.stringify(rodadaEsperada1) === JSON.stringify(rodada1)) {
     console.log('1: NÃO-OK');
 }
 
-const rodada2 = gerarRodada(rodadas);
-rodadas.push(rodada2);
+const rodada2 = gerarRodada(rodadasGeral);
+rodadasGeral.push(rodada2);
 
-const rodadaEsperada2 = [[ 1, 3 ], [ 2, 4 ]];
+const rodadaEsperada2 = [[1, 3], [2, 4]];
 if (JSON.stringify(rodadaEsperada2) === JSON.stringify(rodada2)) {
-  console.log('2: OK');
+    console.log('2: OK');
 } else {
-  console.log('2: NÃO-OK');
-  console.log(rodada2);
+    console.log('2: NÃO-OK');
+    console.log(rodada2);
+}
+const rodada3 = gerarRodada(rodadasGeral);
+rodadasGeral.push(rodada3);
+
+const rodadaEsperada3 = [[1, 4], [2, 3]];
+if (JSON.stringify(rodadaEsperada3) === JSON.stringify(rodada3)) {
+    console.log('2: OK');
+} else {
+    console.log('3: NÃO-OK');
+    console.log(rodada2);
 }
 
