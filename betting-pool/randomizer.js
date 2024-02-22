44fe0c241cb0dba7d8ed7e802cda58d0cb14f578
Pereirapedr0 <132013108+Pereirapedr0@users.noreply.github.com>
2024-02-22 20:22:25 -0300

    logica das partidas em html

--- betting-pool/randomizer.js
+++ betting-pool/randomizer.js
@@ -1,29 +1,25 @@
-const partidas = [];
-
-// função recursiva
 function gerarRodada(partidas) {
-    const rodadaSendoConstruida = []
-    const times = [1, 2, 3, 4];
+    const rodadaSendoConstruida = [] 
     const timesJaNaRodada = [];
 
     for (let i = 0; i < times.length; i++) {
         for (let j = i + 1; j < times.length; j++) {
-            const timesNaoEstaoNaRodada = !timesJaNaRodada.includes(times[i]) && !timesJaNaRodada.includes(times[j]);
+            const timesNaoEstaoNaRodada = !timesJaNaRodada.includes(times[i].nome) && !timesJaNaRodada.includes(times[j].nome);
             if (timesNaoEstaoNaRodada) {
                 if (partidas.length === 0) {
-                    rodadaSendoConstruida.push([times[i], times[j]]);
-                    timesJaNaRodada.push(times[i]);
-                    timesJaNaRodada.push(times[j]);
+                    rodadaSendoConstruida.push([times[i].nome, times[j].nome]);
+                    timesJaNaRodada.push(times[i].nome);
+                    timesJaNaRodada.push(times[j].nome);
                 } else {
-                    const partida = [times[i], times[j]];
+                    const partida = [times[i].nome, times[j].nome];
                     const jaAconteceu = partidas.some(partidaAnterior =>
                         JSON.stringify(partida) === JSON.stringify(partidaAnterior)
                     );
 
                     if (!jaAconteceu) {
-                        rodadaSendoConstruida.push([times[i], times[j]]);
-                        timesJaNaRodada.push(times[i]);
-                        timesJaNaRodada.push(times[j]);
+                        rodadaSendoConstruida.push([times[i].nome, times[j].nome]);
+                        timesJaNaRodada.push(times[i].nome);
+                        timesJaNaRodada.push(times[j].nome);
                     }
                 }
             }
@@ -32,38 +28,3 @@ function gerarRodada(partidas) {
 
     return rodadaSendoConstruida;
 }
-
-
-// refatorar pra não repetir código: Criar uma engine teste.
-const rodada1 = gerarRodada(partidas);
-partidas.push(...rodada1);
-
-const rodadaEsperada1 = [[1, 2], [3, 4]];
-if (JSON.stringify(rodadaEsperada1) === JSON.stringify(rodada1)) {
-    console.log('1: OK');
-} else {
-    console.log(rodada1);
-    console.log('1: NÃO-OK');
-}
-
-const rodada2 = gerarRodada(partidas);
-partidas.push(...rodada2);
-
-const rodadaEsperada2 = [[1, 3], [2, 4]];
-if (JSON.stringify(rodadaEsperada2) === JSON.stringify(rodada2)) {
-    console.log('2: OK');
-} else {
-    console.log('2: NÃO-OK');
-    console.log(rodada2);
-}
-const rodada3 = gerarRodada(partidas);
-partidas.push(...rodada3);
-
-const rodadaEsperada3 = [[1, 4], [2, 3]];
-if (JSON.stringify(rodadaEsperada3) === JSON.stringify(rodada3)) {
-    console.log('3: OK');
-} else {
-    console.log('3: NÃO-OK');
-    console.log(rodada3);
-}
-
